dc822631e89ce2a5d589415a06c004be27999636
rewqlleq <flamexsnow21@gmail.com>
2025-09-03 11:42:22 +0300

    Add files via upload

---
+++ script thonny4py.py
@@ -0,0 +1,49 @@
+print("hello world" "1")
+print ("1"+"1")
+#print(1+2) #keelatud
+#"hello world" - str
+ #1 - int
+#1.5 - float
+
+#muutujad
+name ="Beyond"
+perekonanimi = "birthday"
+print(name + perekonanimi)
+
+#luua muutjad film,aasta,rezisöör ja naitlejal  ja naitleja 2
+#tulemus: film matrix ilmus aastal 1999, rezisöör on wachovski,
+#ning peanäitlejad on Keanu Reeves ja Laurence  Fishburne
+film= "matrix"
+year= 1999
+rezisöör="wachovski"
+naitleja1="Keanu Reeves"
+naitleja2="Laurence Fishburne"
+
+print( "film" + film + "ilmus aastal" + "rezissöör on" + rezisöör + ning peanäitlejad )
+        
+
+lemmikloom = input ( "rebane:" )
+print ( "lemmik loom" + lemmikloom )
+#str() -text -arv
+#int() -arv - text
+
+
+
+pikkus= input("sisseta pikkus") pikkus
+laius= input"sisseta laius"
+umbrmot2 = 2 * (pikkus + laius)
+pindala =pikkus laius("ümbermõõt ",umbermot)
+print("laius: ", laius)
+      #Kirjuta programm mis küsibasutajalt jargmised andmed:
+#    nimi Beyond
+#    elukoht 
+#    lemmikfilm
+#    lemmikmuusik
+#   kolm lemmiktoitu
+#
+      
+#1 salvesta iga sastus eraldi muutujasse
+#2väljasta tervituteksts, kus kasutakse nimi ja vanus
+#3väljasta lause elukoha  kohta
+#4väljasta lause lemmikfilm  kotha ja muusika kohta
+#5väljasta kõik toidud Ühes lauses. (minu lemmiktoidud on ?,? ja ?)
