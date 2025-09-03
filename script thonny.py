8779d043ab1b3492d4ae42a913672ddff8b84ac7
rewqlleq <flamexsnow21@gmail.com>
2025-09-03 10:34:04 +0300

    xddxddd
    script thonny

---
+++ script thonny.py
@@ -0,0 +1,25 @@
+print("hello world" "1")
+print ("1"+"1")
+#print(1+2) #keelatud
+#"hello world" - str
+ #1 - int
+#1.5 - float
+
+#muutujad
+name ="Beyond"
+perekonanimi = "birthday"
+print(name + perekonanimi)
+
+#luua muutjad film,aasta,rezisöör ja naitlejal  ja naitleja 2
+#tulemus: film matrix ilmus aastal 1999, rezisöör on wachovski,
+#ning peanäitlejad on Keanu Reeves ja Laurence  Fishburne
+film= "matrix"
+year= 1999
+rezisöör="wachovski"
+naitleja1="Keanu Reeves"
+naitleja2="Laurence Fishburne"
+print("Film" + "matrix"+ "talvel" + year  "rezisöör "
+        ning peanäitlejad on "wachovski" ) 
+
+lemmikloom = input("rebane:")
+print ("lemmik loom" 
