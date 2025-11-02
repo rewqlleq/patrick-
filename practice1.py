6082c2d903ff99c8e55f6961afb72b6af96c907d
rewqlleq <flamexsnow21@gmail.com>
2025-11-03 00:25:40 +0200

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,5 @@
+arr = ["1","2","3", "4" ]
+
+print(arr[1 ] )
+for el in arr:
+    print(el)
---
+++ practice2.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+numbrid = [5, 10, 15]
+summa = 0
+
+for n in numbrid:
+    summa += n
+
+print("Qxxo:", summa)
